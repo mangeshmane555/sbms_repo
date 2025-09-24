74b6c34085b247a8a8c994fa6c3cf06ba5b630d7
mangeshmane555 <mangeshmane555@gmail.com>
2025-09-24 11:33:55 +0530

    added some changes to the greet api-> pom.xml, greet->controller, greet-> .yml file

--- 33_SB_MS_Greet_Api/src/main/java/in/mane/rest/GreetRestController.java
+++ 33_SB_MS_Greet_Api/src/main/java/in/mane/rest/GreetRestController.java
@@ -1,15 +1,23 @@
 package in.mane.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetRestController {
 	
+	@Autowired
+	Environment env;
+	
 	@GetMapping("/greet")
 	public String greetMsg() {
 		
-		return "Good Evening";
+		// To check in which port app is running, get that server port number 
+		String port = env.getProperty("server.port");
+		
+		return "Good Evening ("+port+")";
 		
 	}
 
